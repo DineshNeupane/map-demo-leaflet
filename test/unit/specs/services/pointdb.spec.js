a37c9e02aeb3623e7a6c51f65a491498ab63558d
Max Prettyjohns <heshoots9999@gmail.com>
2017-07-10 15:07:14 +0100

    Add test for pointdb

---
+++ test/unit/specs/services/pointdb.spec.js
@@ -0,0 +1,94 @@
+/* eslint-disable */
+var pointdb = require('../../../../src/services/pointdb.js');
+const request = require('superagent');
+const Promise = require('bluebird');
+
+describe('dateTimeHandler', () => {
+  it('should accept correct date times', () => {
+    return pointdb.dateTimeHandler('2017-05-05', '20-30').then((datestr) => {
+      expect(datestr).to.equal('2017-05-05/20-30');
+    })
+  });
+
+  it('should reject incorrect dates', () => {
+    return pointdb.dateTimeHandler('2017-05-32', '20-30').catch((datestr) => {
+      expect(datestr).to.equal('Invalid date');
+    })
+  });
+
+  it('should reject incorrect times', () => {
+    return pointdb.dateTimeHandler('2017-05-30', '24-30')
+      .catch((datestr) => {
+        expect(datestr).to.equal('Invalid date');
+      })
+  });
+});
+
+describe('getPoints', function(){
+  before(function(){
+    sinon
+      .stub(request, 'get')
+      .returns({
+        accept: function (blah) {
+          return Promise.resolve({body: {data: 'testdata'}});
+        }
+      });
+  });
+
+  after(function(){
+    request.get.restore();
+  });
+
+  it('should return data from response', function(){
+    return pointdb.getPoints('2017-05-30', '23-00')
+      .then((json) => {
+        expect(json).to.equal('testdata');
+      })
+  });
+});
+
+describe('getLevels', function(){
+  before(function(){
+    sinon
+      .stub(request, 'get')
+      .returns({
+        accept: function (blah) {
+          return Promise.resolve({body: {data: 'testdata'}});
+        }
+      });
+  });
+
+  after(function(){
+    request.get.restore();
+  });
+
+  it('should return data from response', function(){
+    return pointdb.getLevels('2017-05-30', '23-00')
+      .then((json) => {
+        expect(json).to.equal('testdata');
+      })
+  });
+});
+
+describe('getTide', function(){
+  before(function(){
+    sinon
+      .stub(request, 'get')
+      .returns({
+        accept: function (blah) {
+          return Promise.resolve({body: {data: 'testdata'}});
+        }
+      });
+  });
+
+  after(function(){
+    request.get.restore();
+  });
+
+  it('should return data from response', function(){
+    return pointdb.getTide('2017-05-30', '23-00')
+      .then((json) => {
+        expect(json).to.equal('testdata');
+      })
+  });
+});
