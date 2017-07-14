510ef25d81f05cce035faa60ac0bac99791775a7
Max Prettyjohns <heshoots9999@gmail.com>
2017-07-14 11:23:59 +0100

    Allow pulling from API for specific day

---
+++ src/model/pointStore.js
@@ -0,0 +1,80 @@
+import { readingArrayToDataPoints } from '../model/stations';
+
+const _ = require('lodash');
+
+function binReadings(readings) {
+  const bins = {};
+  readings.map((reading) => {
+    if (bins[reading.moment()]) {
+      bins[reading.moment()].push(reading);
+    } else {
+      bins[reading.moment()] = [reading];
+    }
+    return null;
+  });
+  console.log(bins);
+  return bins;
+}
+
+/** Encapsulate a set of points for dates **/
+export default class PointStore {
+  constructor(limit, getterFunction, locations) {
+    this.limit = limit;
+    this.getterFunction = getterFunction;
+    this.points = {};
+    this.loadingDays = [];
+    this.locations = locations;
+  }
+
+
+  getDay(date) {
+    if (!this.loadingDays[date]) {
+      console.log('getting day');
+      this.loadingDays[date] = true;
+      const getPromises = [];
+      for (let i = 0; i < this.limit; i += 10000) {
+        // eslint-disable-next-line
+        getPromises.push(this.getterFunction({ date, '_offset': i }));
+      }
+      return Promise.all(getPromises)
+        .then((out) => {
+          console.log(out);
+          return out;
+        })
+        .then(out =>
+          out.reduce((all, page) =>
+            all.concat(page)
+          , []),
+        )
+        .then(readings =>
+          binReadings(readings))
+        .then((result) => {
+          const out = {};
+          Object.keys(result).map((key) => {
+            out[key] = readingArrayToDataPoints(this.locations, result[key]);
+            return null;
+          });
+          return out;
+        })
+        .then((result) => {
+          this.points = _.extend(this.points, result);
+        });
+    }
+    return Promise.resolve(true);
+  }
+
+  getLatest() {
+    return this.getterFunction({ latest: '' })
+      .then(readings => readingArrayToDataPoints(this.locations, readings));
+  }
+
+  getPoint(date) {
+    let pointPromise;
+    if (this.points[date]) {
+      pointPromise = Promise.resolve(this.points[date]);
+    } else {
+      pointPromise = Promise.resolve([]);
+    }
+    return pointPromise;
+  }
+}
